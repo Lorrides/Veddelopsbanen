e4511d6679905b0c5b2a091cf84bf0672b824159
Lorrides <tarhh@outlook.com>
2016-01-22 12:07:46 +0100

    Lagt til logikk i greyhound klassen, (test logikk)

--- Greyhound.cs
+++ Greyhound.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Veddeløp
 {
     public class Greyhound
     {
-        public int StartingPosition;
-        public int RacetrackLength;
-        public PictureBox MyPictureBox = null;
+        public int StartPosition;
+        public int BaneLengde;
+        public PictureBox BildeBox = null;
         public int Location = 0;
-        public Random Randomizer;
+        public Random Tilfeldig;
 
        public bool Run()
         {
-            return true;
+            Location += Tilfeldig.Next(1, 5);
+            BildeBox.Left = Location;
+            if (BildeBox.Left >= BaneLengde) { return true; }
+            else
+            {
+                return false;
+            }
         }
 
         public void TakeStartingPosition()
         {
-
+            Location = 0;
+            BildeBox.Left = StartPosition;
         }
     }
 }
