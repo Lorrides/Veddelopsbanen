e2db6123bb3d9e1e2a36963d8175f1c12787d2ac
Lorrides <tarhh@outlook.com>
2016-01-22 12:14:34 +0100

    Oppdatert logikken for person som better

--- Guy.cs
+++ Guy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Veddeløp
 {
@@ -17,12 +18,34 @@ namespace Veddeløp
 
         public void UpdateLabels()
         {
+            MYRadoiButton.Text = Name + " has " + Cash + " kroner ";
+            MyLabel.Text = Name + " har ikke plasert et veddemål ";
 
         }
 
-        public void ClearBet() { }
+        public void ClearBet() {
+            MyBet.Amount = 0;
+            MyBet.Dog = 0;
+            MyBet.Bettor = this;
+        }
 
-        public void Collect (int Winner) { }
+        public bool PlaceBet (int BetAmount, int DogToWin)
+        {
+            if (Cash >= BetAmount)
+            {
+                MyBet.Amount = BetAmount;
+                MyBet.Dog = DogToWin;
+                MyBet.Bettor = this;
+                return true;
+            }
+            return false;
+        }
+
+        public void Collect (int winner)
+        {
+            Cash += MyBet.PayOut(winner);
+            this.UpdateLabels();
+        }
 
 
     }
